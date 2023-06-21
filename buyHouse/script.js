46eee84022d99e77e0d9ff97cc7be2ef002063d9
umideev <umidjondev35@gmail.com>
2023-06-21 15:04:28 +0500

    Aka gsapni to'g'irlab bering please!

---
+++ buyHouse/script.js
@@ -0,0 +1,24 @@
+const h1 = document.getElementById('h1')
+const p = document.getElementById('p')
+const box = document.getElementById('box')
+const box2 = document.getElementById('box2')
+const logo = document.getElementById('logo')
+
+gsap.to('#h1', {
+    x:50
+})
+
+gsap.to('#p', {
+    x:50
+})
+
+gsap.to('#box', {
+    x:30
+})
+
+gsap.to('#box2', {
+    x:30
+})
+
+
+
